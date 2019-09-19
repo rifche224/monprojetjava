4c305f48c579921e7b9089705fe0a8b9aaaed00c
rifche224 <cherif.sy@hotmail.com>
2019-09-19 02:11:00 +0200

    ceci est mon premierenvoie de code

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+package medecin;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            System.out.println("hello word!!");
+            System.out.println("veuillez entrer votre mot de passe");
+	}
+
+}
+
